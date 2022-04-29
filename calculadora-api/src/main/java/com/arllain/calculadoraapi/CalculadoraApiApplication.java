ff6504f2dbe7c077a8b0e44bbd8879125c2a6211
arllain <arllain@gmail.com>
2022-04-29 17:37:28 -0300

    calculadora-api

---
+++ calculadora-api/src/main/java/com/arllain/calculadoraapi/CalculadoraApiApplication.java
@@ -0,0 +1,14 @@
+package com.arllain.calculadoraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CalculadoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApiApplication.class, args);
+	}
+
+}
